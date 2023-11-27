ac1fa0777c1587da9cd7dfc1d2fd9e46c7e51061
Marcin <marcinstankowsky@gmail.com>
2023-11-27 22:59:41 +0100

    Add project files.

---
+++ EventPulse/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EventPulse;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ EventPulse/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EventPulse;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
